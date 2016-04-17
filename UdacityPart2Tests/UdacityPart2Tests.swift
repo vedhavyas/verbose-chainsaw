e01c08cfa6a4d72797bfa43b669db94e801c8f86
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-04-17 19:18:02 +0530

    Initial Commit

---
+++ UdacityPart2Tests/UdacityPart2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  UdacityPart2Tests.swift
+//  UdacityPart2Tests
+//
+//  Created by Vedhavyas Singareddi on 17/04/16.
+//  Copyright © 2016 Vedhavyas Singareddi. All rights reserved.
+//
+
+import XCTest
+@testable import UdacityPart2
+
+class UdacityPart2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
