12dda13c8f3cf2fb2f5a51038650feb1cbfd0f8d
vedhavyas singareddi <vedhavyas.singareddi@gmail.com>
2016-04-18 23:12:35 +0530

    Initial Commit

--- UdacityPart2/ViewController.swift
+++ UdacityPart2/ViewController.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeColor(sender: AnyObject){
+        let r: CGFloat = CGFloat(redSlider.value)
+        let g: CGFloat = CGFloat(greenSlider.value)
+        let b: CGFloat = CGFloat(blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
 
 
